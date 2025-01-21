6bcdf2a99b33ae99e963e67b2b2e65a94cf74f85
Abhishek8959296536 <abhishekgour738@gmail.com>
2025-01-21 16:13:50 +0530

    21 jan

--- 1.cpp
+++ 1.cpp
@@ -1,31 +1,52 @@
 #include<iostream>
 using namespace std;
-int fibo(){
-    int i,n;
-    int ft=0;
-    int st=1;
-    int next;
-    i=0;n=7;
-    while(i<n)
+// int fibo(){
+//     int i,n;
+//     int ft=0;
+//     int st=1;
+//     int next;
+//     i=0;n=7;
+//     while(i<n)
+//     {
+//         if(i==0)
+//         {
+//             cout<<ft;
+//             i++;
+//         }
+//         if(i==1)
+//         {
+//             cout<<st;
+//             i++;
+//         }
+//         next=ft+st;
+//         ft=st;
+//         st=next;
+//         i++;
+//         cout<<next;
+//     }
+//     return 0;
+//     }
+//     int main(){
+//    fibo();
+//    }
+ 
+ void rev(string str)
+ {
+    int start=0;
+    int end=str.length()-1;
+    while(start<end)
     {
-        if(i==0)
-        {
-            cout<<ft;
-            i++;
-        }
-        if(i==1)
-        {
-            cout<<st;
-            i++;
-        }
-        next=ft+st;
-        ft=st;
-        st=next;
-        i++;
-        cout<<next;
+        char temp=str[start];
+        str[start]=str[end];
+        str[end]=temp;
+        start++;
+        end--;
     }
-    return 0;
-    }
-    int main(){
-   fibo();
-   }
+    cout<<str;
+ }
+ int main()
+ {
+    string str="abhi";
+    rev(str);
+ }
+ 
---
+++ string.cpp
@@ -0,0 +1,54 @@
+#include <iostream>
+using namespace std;
+// void rev(string str)
+//  {
+//     int start=0;
+//     int end=str.length()-1;
+//     while(start<end)
+//     {
+//         char temp=str[start];
+//         str[start]=str[end];
+//         str[end]=temp;
+//         start++;
+//         end--;
+//     }
+//     cout<<str;
+//  }
+//  int main()
+//  {
+//     string str="abhi";
+//     rev(str);
+//  }
+
+// void pal(string str)
+//  {
+//     int start=0;
+//     int end=str.length()-1;
+//     while(start<end)
+//     {
+//         if(str[start]!=str[end])
+//     }
+//     cout<<"no";
+//     return;
+//  }
+//  start++;
+//  end--;
+//  int main()
+//  {
+//     string str="abhi";
+    
+//  }
+
+void rev(string &str)
+ {
+    for(i=0;i<str.length();i++)
+    {
+        if(str[i])
+    }
+
+ }
+ int main()
+ {
+    string str="abhi";
+    rev(str);
+ }
